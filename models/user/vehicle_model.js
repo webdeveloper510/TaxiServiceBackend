18182de89067f9fb6c66e3dbc4b12458aad23d01
webdeveloper510 <webdeveloper510>
2023-10-20 15:48:13 +0530

    vehicle make

--- models/user/vehicle_model.js
+++ models/user/vehicle_model.js
@@ -14,6 +14,10 @@ const vehicle = new Schema({
         type:String,
         default:''
     },
+    vehicle_make:{
+        type:String,
+        default:'Other'
+    },
     AC:{
         type:Boolean,
         default:false
