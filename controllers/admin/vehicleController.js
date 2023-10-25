1b17799e3117e9c5de63c0c899f29096b8491b95
webdeveloper510 <webdeveloper510>
2023-10-25 11:31:27 +0530

    vehicle photo edit

--- controllers/admin/vehicleController.js
+++ controllers/admin/vehicleController.js
@@ -16,7 +16,7 @@ const imageStorage = new CloudinaryStorage({
         folder: "TaxiBooking",
         // allowedFormats: ["jpg", "jpeg", "png"],
         public_id: (req, file) =>
-        `${Date.now()}-${Math.round(Math.random() * 1e9)}`,
+            `${Date.now()}-${Math.round(Math.random() * 1e9)}`,
         // format: async (req, file) => "jpg", // Convert all uploaded images to JPEG format
         // transformation: [{ width: 500, height: 500, crop: "limit" }],
         maxFileSize: 10000000,
@@ -89,7 +89,7 @@ exports.add_vehicle = async (req, res) => {
 
 exports.get_vehicles = async (req, res) => {
     try {
-        let get_vehicle = await VEHICLE.find({is_deleted:false}).sort({ 'createdAt': -1 })
+        let get_vehicle = await VEHICLE.find({ is_deleted: false }).sort({ 'createdAt': -1 })
         if (!get_vehicle) {
             res.send({
                 code: constant.error_code,
@@ -134,66 +134,70 @@ exports.get_vehicle_detail = async (req, res) => {
 }
 
 exports.edit_vehicle = async (req, res) => {
-    try {
-        let data = req.body
-        let criteria = { _id: req.params.id }
-        let option = { new: true }
-        console.log('dafdasf')
-        let check_vehicle = await VEHICLE.findOne({ _id: req.params.id })
-        console.log('dafdasf')
-        if (!check_vehicle) {
+    vehicleUpload(req, res, async (err) => {
+        try {
+            let data = req.body
+            let criteria = { _id: req.params.id }
+            let option = { new: true }
+            console.log('dafdasf')
+            let check_vehicle = await VEHICLE.findOne({ _id: req.params.id })
+            console.log('dafdasf')
+            if (!check_vehicle) {
+                res.send({
+                    code: constant.error_code,
+                    message: "Invalid ID"
+                })
+                return;
+            }
+            data.vehicle_photo = req.file ? req.file.path : check_vehicle.vehicle_photo
+            let updateVehicle = await VEHICLE.findOneAndUpdate(criteria, data, option)
+            if (!updateVehicle) {
+                res.send({
+                    code: constant.error_code,
+                    message: "Unable to update the details"
+                })
+            } else {
+                res.send({
+                    code: constant.success_code,
+                    message: "Updated Successfully",
+                    result: updateVehicle
+                })
+            }
+        } catch (err) {
             res.send({
                 code: constant.error_code,
-                message: "Invalid ID"
+                message: err.message
             })
-            return;
         }
-        let updateVehicle = await VEHICLE.findOneAndUpdate(criteria, data, option)
-        if (!updateVehicle) {
+    })
+
+}
+
+exports.delete_vehicle = async (req, res) => {
+    try {
+        let criteria = { _id: req.params.id };
+        let newValue = {
+            $set: {
+                is_deleted: true
+            }
+        };
+        let option = { new: true }
+        let deleteOption = await VEHICLE.findOneAndUpdate(criteria, newValue, option)
+        if (!deleteOption) {
             res.send({
                 code: constant.error_code,
-                message: "Unable to update the details"
+                message: "Unable to Delete Vehicle"
             })
         } else {
             res.send({
                 code: constant.success_code,
-                message: "Updated Successfully",
-                result: updateVehicle
+                message: "Deleted"
             })
         }
     } catch (err) {
         res.send({
             code: constant.error_code,
             message: err.message
-        })
-    }
-}
-
-exports.delete_vehicle = async(req,res)=>{
-    try{
-        let criteria = {_id:req.params.id};
-        let newValue = {
-            $set:{
-                is_deleted:true
-            }
-        };
-        let option = {new:true}
-        let deleteOption = await VEHICLE.findOneAndUpdate(criteria,newValue,option)
-        if(!deleteOption){
-            res.send({
-                code:constant.error_code,
-                message:"Unable to Delete Vehicle"
-            })
-        }else{
-            res.send({
-                code:constant.success_code,
-                message:"Deleted"
-            })
-        }
-    }catch(err){
-        res.send({
-            code:constant.error_code,
-            message:err.message
         })
     }
 }
