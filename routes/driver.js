14962dc0fc79ffb72b09ee5961caf0e6772c8a31
webdeveloper510 <webdeveloper510>
2023-11-01 17:19:55 +0530

    login driver

---
+++ routes/driver.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var loginController = require('../controllers/admin/loginController')
+var subAdminController = require('../controllers/admin/subAdminController.js');
+var vehicleController = require('../controllers/admin/vehicleController');
+var agencyController = require('../controllers/admin/agencyController');
+var driverController = require('../controllers/subadmin/driverController');
+var fareController = require('../controllers/admin/fareController');
+var tripController = require('../controllers/subadmin/tripController');
+const { verifyToken } = require('../middleware/auth');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/login',driverController.login_driver)
+
+
+module.exports = router;
