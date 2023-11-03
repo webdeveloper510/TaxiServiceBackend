9b92b80b347bf04ddb169c2e384ae5e1e52c4549
webdeveloper510 <webdeveloper510>
2023-11-03 12:43:44 +0530

    enc change

--- config/emailConstant.js
+++ config/emailConstant.js
@@ -1,9 +1,10 @@
+require("dotenv").config()
 module.exports ={
     credentials:{
         service: "gmail",
         auth: {
-            user: "veritywcb23@gmail.com",
-            pass: "vfrz mdsj blaj onlj",
+            user: process.env.userEmail,
+            pass: process.env.userPassword,
         }
     },
     from_email:"brainalywin@gmail.com",
