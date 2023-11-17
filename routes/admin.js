4ae8a8e08932b37969bb7c852767a82e646c826a
webdeveloper510 <webdeveloper510>
2023-11-17 12:48:50 +0530

    data

--- routes/admin.js
+++ routes/admin.js
@@ -36,6 +36,7 @@ router.delete('/delete_sub_admin/:id',[verifyToken],subAdminController.delete_su
 router.get('/get_vehicle_types',[verifyToken],vehicleController.get_vehicle_types)
 router.post('/add_vehicle',[verifyToken],vehicleController.add_vehicle)
 router.get('/get_vehicles',[verifyToken],vehicleController.get_vehicles)
+router.get('/get_vehicles_with_type/:vehicle_type',[verifyToken],vehicleController.get_vehicles_with_type)
 router.get('/get_vehicle_type/:vehicle_type',[verifyToken],vehicleController.get_vehicle_type)
 router.put('/edit_vehicle/:id',[verifyToken],vehicleController.edit_vehicle)
 router.delete('/delete_vehicle/:id',[verifyToken],vehicleController.delete_vehicle)
