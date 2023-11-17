e6667e0dd9a44631eef43b60c13c949b0b8cfca5
webdeveloper510 <webdeveloper510>
2023-11-17 14:55:14 +0530

    new key

--- config/vehicleType.js
+++ config/vehicleType.js
@@ -1,66 +1,63 @@
-module.exports =  [
-    // {
-    //   "name": "Compact Car",
-    //   "seats": 5
-    // },
-    // {
-    //   "name": "Sedan",
-    //   "seats": 5
-    // },
-    // {
-    //   "name": "SUV",
-    //   "seats": 9
-    // },
-    // {
-    //   "name": "Minivan",
-    //   "seats": 8
-    // },
-    // {
-    //   "name": "Full-Size Van",
-    //   "seats": 15
-    // },
-    // {
-    //   "name": "Pickup Truck",
-    //   "seats": 6
-    // },
-    // {
-    //   "name": "Sports Car",
-    //   "seats": 2
-    // },
-    // {
-    //   "name": "Convertible",
-    //   "seats": 2
-    // },
-    // {
-    //   "name": "Truck",
-    //   "seats": 3
-    // },
-    // {
-    //   "name": "Bus",
-    //   "seats": 60
-    // }
-    {
-      name:"Sedan",
-      seats:0,
-      start_price:3.60,
-      per_km:2.65,
-      per_min:0.44
-    },
-    {
-      name:"Minivan",
-      seats:0,
-      start_price:7.33,
-      per_km:3.34,
-      per_min:0.49
-    },
-    {
-      name:"Luxury",
-      seats:0,
-      start_price:30.49,
-      per_km:7.49,
-      per_min:4.29
-    }
-
-
-
-  ];
+module.exports = [
+  // {
+  //   "name": "Compact Car",
+  //   "seats": 5
+  // },
+  // {
+  //   "name": "Sedan",
+  //   "seats": 5
+  // },
+  // {
+  //   "name": "SUV",
+  //   "seats": 9
+  // },
+  // {
+  //   "name": "Minivan",
+  //   "seats": 8
+  // },
+  // {
+  //   "name": "Full-Size Van",
+  //   "seats": 15
+  // },
+  // {
+  //   "name": "Pickup Truck",
+  //   "seats": 6
+  // },
+  // {
+  //   "name": "Sports Car",
+  //   "seats": 2
+  // },
+  // {
+  //   "name": "Convertible",
+  //   "seats": 2
+  // },
+  // {
+  //   "name": "Truck",
+  //   "seats": 3
+  // },
+  // {
+  //   "name": "Bus",
+  //   "seats": 60
+  // }
+  {
+    name: "Sedan",
+    seats: 0,
+    start_price: 3.60,
+    per_km: 2.65,
+    per_min: 0.44
+  },
+  {
+    name: "Minivan",
+    seats: 0,
+    start_price: 7.33,
+    per_km: 3.34,
+    per_min: 0.49
+  },
+  {
+    name: "Luxury",
+    seats: 0,
+    start_price: 30.49,
+    per_km: 7.49,
+    per_min: 4.29
+  }
+];
