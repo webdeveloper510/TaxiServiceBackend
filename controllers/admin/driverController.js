49202c2e4be66e33f3c50039a4562491901a4fe5
webdeveloper510 <webdeveloper510>
2023-11-23 13:14:04 +0530

    company

--- controllers/admin/driverController.js
+++ controllers/admin/driverController.js
@@ -274,6 +274,7 @@ exports.update_driver = async (req, res) => {
             }
             req.body.profile_image = driver_image.length != 0 ? driver_image[0] : existingDriver.profile_image
             req.body.driver_documents = driver_documents.length != 0 ? driver_documents[0] : existingDriver.driver_documents
+            updates.email = existingDriver.email
             const updatedDriver = await DRIVER.findOneAndUpdate({ _id: driverId }, updates, { new: true });
             if (updatedDriver) {
                 res.send({
