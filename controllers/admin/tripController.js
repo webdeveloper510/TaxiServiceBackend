40c3ff698992d50024fdd8cffa960f351abc6d51
ravi <ravi@codenomad.net>
2024-07-05 14:38:28 +0530

    done

--- controllers/admin/tripController.js
+++ controllers/admin/tripController.js
@@ -819,7 +819,7 @@ exports.alocate_driver = async (req, res) => {
             } else {
                 try {
                     
-                    if(check_driver._id.toString() != req?.user?.driverId?.toString()){await sendNotification(check_driver.deviceToken,"New Trip is allocated have ID "+update_trip.trip_id ,"New Trip is allocated have ID "+update_trip.trip_id, update_trip )
+                    if(check_driver._id.toString() != req?.user?.driverId?.toString() && data.status !== "Booked"){await sendNotification(check_driver.deviceToken,"New Trip is allocated have ID "+update_trip.trip_id ,"New Trip is allocated have ID "+update_trip.trip_id, update_trip )
                     }
                 } catch (error) {
                     console.log("🚀 ~ exports.alocate_driver= ~ error: Unable to send notification", error)
