bc46f58ddb549d5b9a6653fb9045a03147278eaf
webdeveloper510 <developerweb6@gmail.com>
2024-11-29 11:58:30 +0530

    admin company trips added api

--- controllers/admin/tripController.js
+++ controllers/admin/tripController.js
@@ -555,6 +555,209 @@ exports.get_trip = async (req, res) => {
   }
 };
 
+exports.companyGetTrip = async (req, res) => {
+  try {
+    let data = req.body;
+    let companyId = req.body.company_id;
+    let companydata = await USER.findOne({ role: "COMPANY", _id: companyId });
+
+    if (!companyId || !companydata) {
+
+      return res.send({
+        code: constant.error_code,
+        message: "Invalid company",
+      });
+    } 
+      
+    let mid = new mongoose.Types.ObjectId(companyId);
+    let getIds = await USER.find({ role: "HOTEL", created_by: companyId });
+
+    let search_value = data.comment ? data.comment : "";
+    let ids = [];
+    for (let i of getIds) {
+      ids.push(i._id);
+    }
+    let dateFilter = data.dateFilter; // Corrected variable name
+    if (!['all', 'this_week', 'this_month', 'this_year', 'dateRange'].includes(dateFilter)) {
+      dateFilter = "all";
+    }
+
+    // Update the query based on the date filter
+    let dateQuery = {};
+    if (dateFilter !== "all") {
+      let startDate, endDate;
+      const today = new Date();
+      switch (dateFilter) {
+        case "this_week":
+          const todayDay = today.getDay();
+          startDate = new Date(
+            today.getFullYear(),
+            today.getMonth(),
+            today.getDate() - todayDay
+          );
+          endDate = new Date(
+            today.getFullYear(),
+            today.getMonth(),
+            today.getDate() + (6 - todayDay)
+          );
+          break;
+        case "this_month":
+          startDate = new Date(today.getFullYear(), today.getMonth(), 1);
+          endDate = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+          break;
+        case "this_year":
+          startDate = new Date(today.getFullYear(), 0, 1);
+          endDate = new Date(today.getFullYear(), 11, 31);
+          break;
+        case "dateRange":
+          startDate = new Date(req.body.startDate);
+          endDate = new Date(req.body.endDate);
+
+          // Modify the Date object with setHours
+          
+        default:
+          break;
+      }
+
+      startDate.setUTCHours(0, 0, 1, 0);
+      endDate.setUTCHours(23, 59, 59, 999);
+
+      // Convert the Date objects to ISO 8601 strings
+      startDate = startDate.toISOString();
+      endDate = endDate.toISOString();
+
+      dateQuery = { createdAt: { $gte: new Date(startDate), $lte: new Date(endDate) } };
+    }
+
+    // return res.send({
+    //   code: constant.error_code,
+    //   message: data,
+    //   param: req.params,
+    //   dateQuery: dateQuery,
+    //   // companydata: companydata
+    // });
+
+    const objectIds = ids.map((id) => new mongoose.Types.ObjectId(id));
+
+    let get_trip = await TRIP.aggregate([
+      {
+        $match: {
+          $and: [
+            {
+              $or: [{ created_by: { $in: objectIds } }, { created_by: mid }],
+            },
+            { status: true },
+            { trip_status: req.params.status },
+            { is_deleted: false },
+            dateQuery,
+          ],
+        },
+      },
+      {
+        $lookup: {
+          from: "drivers",
+          localField: "driver_name",
+          foreignField: "_id",
+          as: "driver",
+        },
+      },
+      {
+        $lookup: {
+          from: "vehicles",
+          localField: "vehicle",
+          foreignField: "_id",
+          as: "vehicle",
+        },
+      },
+
+      {
+        $lookup: {
+          from: "agencies",
+          localField: "created_by_company_id",
+          foreignField: "user_id",
+          as: "userData",
+        },
+      },
+      {
+        $lookup: {
+          from: "agencies",
+          localField: "hotel_id",
+          foreignField: "user_id",
+          as: "hotelData",
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          trip_from: 1,
+          trip_to: 1,
+          pickup_date_time: 1,
+          trip_status: 1,
+          createdAt: 1,
+          created_by: 1,
+          status: 1,
+          passenger_detail: 1,
+          vehicle_type: 1,
+          comment: 1,
+          commission: 1,
+          pay_option: 1,
+          customerDetails: 1,
+          price: 1,
+          passengerCount: 1,
+          hotel_name: { $arrayElemAt: ["$hotelData.company_name", 0] },
+          company_name: { $arrayElemAt: ["$userData.company_name", 0] },
+          driver_name: {
+            $concat: [
+              { $arrayElemAt: ["$driver.first_name", 0] },
+              " ",
+              { $arrayElemAt: ["$driver.last_name", 0] },
+            ],
+          },
+          driver_id: { $arrayElemAt: ["$driver._id", 0] },
+          vehicle: {
+            $concat: [
+              { $arrayElemAt: ["$vehicle.vehicle_number", 0] },
+              " ",
+              { $arrayElemAt: ["$vehicle.vehicle_model", 0] },
+            ],
+          },
+          trip_id: 1,
+        },
+      },
+      {
+        $match: {
+          $or: [
+            { comment: { $regex: search_value, $options: "i" } },
+            { trip_id: { $regex: search_value, $options: "i" } },
+            { driver_name: { $regex: search_value, $options: "i" } },
+            { "trip_from.address": { $regex: search_value, $options: "i" } },
+            { "trip_to.address": { $regex: search_value, $options: "i" } },
+            { company_name: { $regex: search_value, $options: "i" } },
+          ],
+        },
+      },
+    ]).sort({ createdAt: -1 });
+    if (!get_trip) {
+      res.send({
+        code: constant.error_code,
+        message: "Unable to get the trips",
+      });
+    } else {
+      res.send({
+        code: constant.success_code,
+        message: "Success",
+        dateQuery:dateQuery,
+        result: get_trip,
+      });
+    }
+  } catch (err) {
+    res.send({
+      code: constant.error_code,
+      message: err.message,
+    });
+  }
+};
+
 exports.get_access_trip = async (req, res) => {
   try {
     if (req.user.role == "DRIVER") {
