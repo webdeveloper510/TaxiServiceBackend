1ea373e0971c4656da5538efdee5dd62945489cc
developer <developerweb6@gmail.com>
2025-02-12 20:20:22 +0530

    code updated in subscription

--- index.js
+++ index.js
@@ -57,6 +57,16 @@ app.post( "/subscription_webhook", bodyParser.raw({type: 'application/json'}), a
             return;
           }
 
+          let logs_data = {
+            api_name: 'subscription_webhook',
+            payload: JSON.stringify(event),
+            error_message: event.type,
+            error_response: JSON.stringify(event)
+            
+          };
+          const logEntry = new LOGS(logs_data);
+          await logEntry.save();
+
           if (event.type === 'invoice.payment_succeeded') {
             const invoice = event.data.object;
 
