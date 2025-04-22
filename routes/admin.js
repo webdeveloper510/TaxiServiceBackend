72b40cc8e7ca27e1959b97ed3e3e2abf22ac3ad0
webdeveloper510 <developerweb6@gmail.com>
2025-04-22 18:54:32 +0530

    code updated for sorting

--- routes/admin.js
+++ routes/admin.js
@@ -130,19 +130,11 @@ router.get("/get_active_drivers",[verifyToken],driverController.get_active_drive
 
 // fare api's
 router.post("/add_fare", [verifyToken], fareController.add_fare);
-router.post(
-  "/access_get_fares",
-  [verifyToken],
-  fareController.access_get_fares
-);
+router.post("/access_get_fares",[verifyToken],fareController.access_get_fares);
 router.get("/get_fares", [verifyToken], fareController.get_fares);
 router.get("/company_get_fares/:company_id", [verifyToken], fareController.companyGetFares);
 router.get("/get_fares/:id", fareController.get_fares);
-router.get(
-  "/get_fare_detail/:id",
-  [verifyToken],
-  fareController.get_fare_detail
-);
+router.get("/get_fare_detail/:id",[verifyToken],fareController.get_fare_detail);
 router.delete("/delete_fare/:id", [verifyToken], fareController.delete_fare);
 router.put("/edit_fare/:id", [verifyToken], fareController.edit_fare);
 
