231b5c231aeecc0096cb559b45e56ba0aa758918
developer <developerweb6@gmail.com>
2025-08-07 00:12:35 +0530

    code updated in get trip

--- controllers/admin/tripController.js
+++ controllers/admin/tripController.js
@@ -777,6 +777,7 @@ exports.get_trip = async (req, res) => {
           is_paid:1,
           car_type:1,
           car_type_id:1,
+          drop_time:1,
           company_trip_payout_status:1,
           hotel_name: { $arrayElemAt: ["$hotelData.company_name", 0] },
           company_name: { $arrayElemAt: ["$userData.company_name", 0] },
