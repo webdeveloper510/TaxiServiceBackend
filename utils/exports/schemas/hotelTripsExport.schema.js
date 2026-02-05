d55a6efaa5331ce74b735d2d075050b5ff9fe4ba
developer <developerweb6@gmail.com>
2026-02-05 06:39:48 +0530

    code uypdated to change miles into km

--- utils/exports/schemas/hotelTripsExport.schema.js
+++ utils/exports/schemas/hotelTripsExport.schema.js
@@ -14,19 +14,25 @@ exports.getHotelTripsExportSchema = () => {
     { header: "Pay Option", key: "pay_option", width: 16 },
     // { header: "Commission Type", key: "commission_type", width: 18 },
     // { header: "Commission Value", key: "commission_value", width: 18 },
-    { header: "Distance (Miles)", key: "trip_distance", width: 14 },
+    { header: "Distance (Km)", key: "trip_distance", width: 14 },
     { header: "Room No", key: "roomNumber", width: 12 },
     // { header: "Created At", key: "createdAt", width: 22, numFmt: "dd-mm-yyyy hh:mm" },
     { header: "Comment", key: "comment", width: 30 },
   ];
 
+  const milesToKm = (miles) => {
+    return miles ? (Number(miles) * 1.609344).toFixed(2) : "";
+  };
   const rowMapper = (doc) => {
 
     // const c = commissionToText(doc.commission);
     const pickup_address = doc?.trip_from?.address || "";
     const drop_address = doc?.trip_to?.address || "";
     const pickup_date_time = formatUtcToLocalTime(doc.pickup_date_time , doc.pickup_timezone , "dd-LL-yyyy HH:mm");
-    
+    const trip_distance = milesToKm(doc.trip_distance);
+   
+
+
     return {
       trip_id: doc.trip_id || "",
       trip_status: doc.trip_status || "",
@@ -39,7 +45,7 @@ exports.getHotelTripsExportSchema = () => {
       pay_option: doc.pay_option || "",
     //   commission_type: c.type,
     //   commission_value: c.value,
-      trip_distance: doc.trip_distance ?? "",
+      trip_distance: trip_distance ?? "",
       roomNumber: doc.roomNumber ?? "",
       // createdAt: doc.createdAt ? new Date(doc.createdAt) : "",
       comment: doc.comment || "",
